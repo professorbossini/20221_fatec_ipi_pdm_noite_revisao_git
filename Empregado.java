b5e94e184370ddbf70fbb65155ea2d58de708649
Rodrigo Bossini <professorbossini@gmail.com>
2022-02-11 21:04:08 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,23 @@
 public class Empregado{
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario () {
+        if (tipo == 1){
+            return salario;
+        }
+        else if (tipo == 2){
+            return salario + salario * comissao;
+        }
+        else if (tipo == 3){
+            return salario + bonus;
+        }
+        return 0;
+    }
 
     public String getNome() {
         return nome;
